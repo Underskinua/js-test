5b13ba81201f0d31620ba9c25ee786a11a393fcf
Underskinua <zovadim@gmail.com>
2023-01-24 18:32:40 +0200

    24-01

--- js-test.js
+++ js-test.js
@@ -70,24 +70,18 @@
 // console.log(makeOrderMessage(2, 100, 50));
 // console.log(makeOrderMessage(4, 300, 100));
 // console.log(makeOrderMessage(10, 70, 200));
-function isAdult(age) {
-  // Change code below this line
-  let isAdult = 20;
-  isAdult = 14;
-  isAdult = 8;
-  isAdult = 37;
-  let passed = isAdult >= 18;
-  passed = isAdult <= 18;
 
-  return passed;
+// function isAdult(age) {
+// const passed = age >= 18;
 
-  // Change code above this line
-}
+// return passed;
+// }
 
-console.log(isAdult(20) >= passed);
-console.log(isAdult(14) >= passed);
-console.log(isAdult(8) >= passed);
-console.log(isAdult(37) >= passed);
+// console.log(isAdult(20) >= 18);
+// console.log(isAdult(14) < 18);
+// console.log(isAdult(8) > 18);
+// console.log(isAdult(37) >= 18);
+
 // const a = 2;
 // const b = 5;
 
@@ -100,3 +94,107 @@ console.log(isAdult(37) >= passed);
 // console.log(b < a); // false
 // console.log(a <= b); // true
 // console.log(b <= a); // false
+
+// function isValidPassword(password) {
+// const SAVED_PASSWORD = "jqueryismyjam";
+// const isMatch = password === "jqueryismyjam";
+
+// return isMatch;
+// }
+
+// console.log(isValidPassword("mangodab3st") == "jqueryismyjam");
+// console.log(isValidPassword("kiwirul3z") == "jqueryismyjam");
+// console.log(isValidPassword("jqueryismyjam") == "jqueryismyjam");
+
+// function checkAge(age) {
+// let message;
+
+// if (age >= 18) {
+
+// message = "You are an adult";
+// } else {
+// if (age < 18) message = "You are a minor";
+// }
+
+// return message;
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+
+// function checkStorage(available, ordered) {
+// let message;
+
+// if (available >= ordered) {
+// message = "Order is processed, our manager will contact you.";
+//  } else {
+//  if (available <= ordered) message = "Not enough goods in stock!";
+//  }
+
+// return message;
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 150));
+// console.log(checkStorage(150, 180));
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+// let message;
+
+// let totalPrice = customerCredits - pricePerDroid * orderedQuantity;
+
+// if (totalPrice <= customerCredits) {
+// message = `You ordered ${orderedQuantity} droids, you have ${totalPrice} // credits left`;
+// } else {
+// if (totalPrice <= customerCredits) message = `Insufficient funds!`;
+// }
+
+// return message;
+// }
+
+// console.log(makeTransaction(3000, 5, 23000));
+// console.log(makeTransaction(1000, 3, 15000));
+// console.log(makeTransaction(5000, 10, 8000));
+// console.log(makeTransaction(2000, 8, 10000));
+// console.log(makeTransaction(500, 10, 5000));
+
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Change code below this line
+  let totalPrice = pricePerDroid * orderedQuantity;
+  let credits = customerCredits - totalPrice;
+
+  if (customerCredits <= totalPrice) {
+    message = `Insufficient funds!`;
+  } else {
+    message = `You ordered ${orderedQuantity} droids, you have ${credits} credits left`;
+  }
+
+  // Change code above this line
+  return message;
+}
+
+console.log(makeTransaction(3000, 5, 23000));
+console.log(makeTransaction(1000, 3, 15000));
+console.log(makeTransaction(5000, 10, 8000));
+console.log(makeTransaction(2000, 8, 10000));
+console.log(makeTransaction(500, 10, 5000));
